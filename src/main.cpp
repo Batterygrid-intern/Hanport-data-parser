b68347efa2d6a33b240acc1e2bc3fbb59ddb9167
Ludde <luddenorin@gmail.com>
2025-10-22 17:20:24 +0200

    updated main with try and catch to catch exceptions from constructor failure

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include "hanport_data.hpp"
 #include <iostream>
+#include <cstdlib>
 
-
-
+#ifndef EX_DATA_PATH
+#define EX_DATA_PATH "ex_data"
+#endif
 int main(int argc, char** argv){
     //call constructor
-    HanportData data_obj("./exampledata/meterdata.bin");
-
-    std::cout << "data_obj instatiated";
-
+    //const char* path_to_testfile = getenv("example_data");
+    try{
+        HanportData data_obj(EX_DATA_PATH);
+    }
+    catch (const std::exception& e){
+        std::cerr << "\nx Error " << e.what() << "\n";
+        return 1;
+    }
     return 0;
-
+}
