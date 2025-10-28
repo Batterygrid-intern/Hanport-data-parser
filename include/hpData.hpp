6780140dc290167bc599b12da37785421dafeaea
Ludde <luddenorin@gmail.com>
2025-10-28 17:49:38 +0100

    created hpdata class added data variables for parsed data and methods to parse the data

--- include/hpData.hpp
+++ include/hpData.hpp
@@ -1,8 +1,65 @@
-
-
+#include <vector>
+#include <string>
 
 class hpData{
     private:
+    std::vector<std::string>  message_array;
+    //time_stamp on the message;
+    float time_stamp = 0;
+    //active energy(energy used to light up a lightbulb run a motor)
+    //(energy that is converted to run devices and is consumed to heat movement or such)
+    float active_enery_import_total = 0;
+    float active_energy_export_total = 0;
+    //reactive energy, energy that is stored in capacitors or not directly consumed by a device) energy that returns in the circut 
+    float reactive_energy_import_total = 0; //kvarh
+    float reactive_energy_export_total = 0; //kvarh
+    //instantaneous(momentan) active effect (total)
+    float active_power_import = 0; //kw
+    float active_power_export = 0; //kw
+    //instantaneous(momentan) reactive effect (total)
+    float reactive_power_import = 0;
+    float reactive_power_export = 0;
+    //active effect per phase(fas) -> (kw)
+    float l1_active_power_import = 0;
+    float l1_active_power_export = 0;
+    float l2_active_power_import = 0;
+    float l2_active_power_export = 0;
+    float l3_active_power_import = 0;
+    float l3_active_power_export = 0;
+    //reactive effect per phase(fas) -> (kw)
+    float l1_reactive_power_import = 0;
+    float l1_reactive_power_export = 0;
+    float l2_reactive_power_import = 0;
+    float l2_reactive_power_export = 0;
+    float l3_reactive_power_import = 0;
+    float l3_reactive_power_export = 0;
+    //phase(fas) voltage(spänning) -> (volt)
+    float l1_voltage_rms = 0;
+    float l2_voltage_rms = 0;
+    float l3_voltage_rms = 0;
+    //current(ampere) ->(ampere - a)
+    float l1_current_rms = 0;
+    float l2_current_rms = 0;
+    float l3_current_rms = 0;
+
+
     public:
+
+    hpData(std::vector<std::string> message__array);
+   
+    //method to parse_message from string array
+    void parse_message();
+    //method to parse the time object
+    void parse_time();
+    //method to parse the electricity data
+    void parse_electricity_data(std::string& line);
+    //method do set the value to value attribute
+    float set_value(std::string& line);
+
+    //transformera värde attribut till dem värden vi vill skicka kw /mw
+
+    //getters
+
+
 
 };
--- src/hpData.cpp
+++ src/hpData.cpp
@@ -1,3 +1,169 @@
 #include "hpData.hpp"
+#include <iostream>
+#include <string>
 
 
+hpData::hpData(std::vector<std::string> message_array){
+    this->message_array = message_array;
+    if(this->message_array.empty()){
+        throw std::runtime_error("object initialisation failed message_array is empty");
+    }
+}
+
+void hpData::parse_message(){
+    //buffer to hold each line extracted from the string array
+    std::string line;
+    //iterate through each line of the string array
+    for(size_t i; i < this->message_array.size();i++){
+        line = this->message_array[i];
+        if(line.find('-') == std::string::npos){
+            continue;
+        }
+        //obis for time_stamp
+        if(line.find("0-0:1.0.0") != std::string::npos){
+            parse_time();
+        }
+        else{
+            parse_electricity_data(line);
+        }
+    }
+}
+void hpData::parse_time(){
+}
+//find electricity obis
+void hpData::parse_electricity_data(std::string& line){
+    //ACTIVE ENERGY
+    //active energy import total
+    if(line.find("1-0:1.8.0")){
+        this->active_enery_import_total = set_value(line);
+    }
+    //active energy export total
+    if(line.find("1-0:2.8.0")){
+        this->active_energy_export_total = set_value(line);
+    } 
+
+    //REACTIVE ENERGY
+    //reactive energy import total
+    if(line.find("1-0:3.8.0")){
+        this->reactive_energy_import_total = set_value(line);
+    }
+    //reactive energy export total
+    if(line.find("1-0:4.8.0")){
+        this->reactive_energy_export_total = set_value(line);
+    }
+
+    //INSTANTANEOUS(MOMENTANT) ACTIVE EFFECT 
+    //active power import
+    if(line.find("1-0:1.7.0")){
+        this->active_power_import = set_value(line);
+    }
+    //active power export
+    if(line.find("1-0:2.7.0")){
+        this->active_power_export = set_value(line);
+    }
+
+    //INSTANTANEOUS(MOMENTANT) REACTIVE EFFECT TOTAL
+    //reactive power import
+    if(line.find("1-0:3.7.0")){
+        this->reactive_power_import = set_value(line);
+    }
+    //reactive power export
+    if(line.find("1-0:4.7.0")){
+        this->reactive_power_export = set_value(line);
+    }
+
+    //ACTIVE EFFECT FOR EACH PHASE 
+    //l1 active power import
+    if(line.find("1-0:21.7.0")){
+        this->l1_active_power_import = set_value(line);
+    }
+    //l1 active power export
+    if(line.find("1-0:22.7.0")){
+        this->l1_active_power_export = set_value(line);
+    }
+    //l2 active_power_import
+    if(line.find("1-0:41.7.0")){
+        this->l2_active_power_import = set_value(line);
+    }
+    //l2 active power export
+    if(line.find("1-0:42.7.0")){
+        this->l2_active_power_export = set_value(line);
+    }
+    //l3 active power import
+    if(line.find("1-0:61.7.0")){
+        this->l3_active_power_import = set_value(line);
+    }
+    //l3 active power export
+    if(line.find("1-0:62.7.0")){
+        this->l3_active_power_export = set_value(line);
+    }
+
+    //REACTIVE EFFEKT FOR EACH PHASE
+    //l1 reactive power import
+    if(line.find("1-0:23.7.0")){
+        this->l1_reactive_power_import = set_value(line);
+    }
+    //l1 reactive power export
+    if(line.find("1-0:24.1.7.0")){
+        this->l1_reactive_power_export = set_value(line);
+    }
+    //l2 reactive power import
+    if(line.find("1-0:43.7.0")){
+        this->l2_reactive_power_import = set_value(line);
+    }
+    //l2 reactive power export
+    if(line.find("1-0:44.7.0")){
+        this->l2_reactive_power_export = set_value(line);
+    }
+    //l3 reactive power import
+    if(line.find("1-0:63.7.0")){
+        this->l3_reactive_power_export = set_value(line);
+    }
+    //l3 reactive power export
+    if(line.find("1-0:64.7.0")){
+        this->l3_reactive_power_export = set_value(line);
+    }
+    
+    //VOLTAGE LEVEL FOR EACH PHASE
+    //l1 voltage rms
+    if(line.find("1-0:32.7.0")){
+        this->l1_voltage_rms = set_value(line);
+    }
+    //l2 voltage rms
+    if(line.find("1-0:52.7.0")){
+        this->l2_voltage_rms = set_value(line);
+    }
+    //l3 voltage rms
+    if(line.find("1-0:72.7.0")){
+        this->l3_voltage_rms = set_value(line);
+    }
+
+    //AMPHERE FOR EACH PHASE
+    //l1 current rms
+    if(line.find("1-0:32.7.0")){
+        this->l1_current_rms = set_value(line);
+    }
+    //l2 current rms
+    if(line.find("1-0:51.7.0")){
+        this->l2_current_rms = set_value(line);
+    }
+    //l3 current rms
+    if(line.find("1-0:71.7.0")){
+        this->l3_current_rms = set_value(line);
+    }
+}
+
+float hpData::set_value(std::string& line){
+    char obis_del =  '(';
+    char unit_del = '*';
+    float value = 0;
+    int start_pos = line.find(obis_del) + 1;
+    int end_pos = line.find(unit_del) - 1;
+    int num_of_chars = end_pos - start_pos;
+    try{
+        float value = std::stof(line.substr(start_pos,num_of_chars));
+    }
+    catch(std::exception& e){
+        throw std::runtime_error("failed to convert string to float for value");
+    }
+}
