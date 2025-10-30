c29c80b0018a5b069e07fc4dcc550d78338611a5
Ludvig <luddenorin@gmail.com>
2025-10-30 16:14:49 +0100

    setup configurations for ReadSerial added

--- include/hpSerialRead.hpp
+++ include/hpSerialRead.hpp
@@ -2,14 +2,36 @@
 #include <fstream>
 #include <vector>
 #include <cstdint>
-#include <CppLinuxSerial/SerialPort.hpp>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BAUD_RATE B1152000
+#define SERIAL_PORT_PATH "/dev/ttyAMA0"
 
 class hpSerialRead{
     private:
-    std::string filepath;
-    std::vector<std::uint8_t> data;
+    char* serial_port_path;
+    struct termios tty;
+    int serial_fd = -1;
+    std::vector<uint8_t> complete_data_message;
+    std::vector<uint8_t> chunk_messages;
+    void hpSetupCflag(struct termios *tty);
+    void hpSetupLflag(struct termios *tty);
+    void hpSetupIflag(struct termios *tty);
+    void hpSetupCc(struct termios *tty);
     public:
-    hpSerialRead(std::string filepath);
+    hpSerialRead();
+    ~hpSerialRead();
+    void openPort();
+    void setupPort();
+    void closePort();
+   
+
+
+
+    /**************************** */
+    hpSerialRead(char* filepath);
     std::ifstream open_fd();
     void read_from_fd(std::ifstream& fd);
     std::vector<uint8_t> getdata(); 
--- src/hpSerialRead.cpp
+++ src/hpSerialRead.cpp
@@ -2,7 +2,67 @@
 #include <iostream>
 #include <cstdint>
 
-/*READ FROM FILE*/
+
+hpSerialRead::hpSerialRead(char* serial_port_path){
+    this->serial_port_path = serial_port_path;
+}
+hpSerialRead::~hpSerialRead(){
+    closePort();
+} 
+void hpSerialRead::openPort(){
+    this->serial_fd = open(this->serial_port_path,O_RDONLY);
+    if(this->serial_fd < 0){
+        throw std::runtime_error("failed to open file descriptor");
+    }
+    setupPort();
+}
+void hpSerialRead::setupPort(){
+    if(tcgetattr(this->serial_fd,&this->tty) != 0){
+        throw std::runtime_error("failed to get attributes for serial port");
+    }
+    hpSetupCflag(&this->tty);
+    hpSetupLflag(&this->tty);
+    hpSetupIflag(&this->tty);
+    hpSetupCc(&this->tty);
+
+    cfsetispeed(&this->tty,BAUD_RATE);
+
+    if(tcsetattr(this->serial_fd,TCSANOW,&this->tty) != 0){
+        throw std::runtime_error("Failed to set attribute to serial port");
+    }
+
+}
+void hpSerialRead::closePort(){
+
+}
+void hpSerialRead::hpSetupCflag(struct termios *tty){
+    tty->c_cflag &= ~PARENB; //disable parity bit 
+    tty->c_cflag &= ~CSTOPB; //set stop bits to 1
+    tty->c_cflag &= ~CSIZE; //clear the size bitmask so you can set the right value for hanport we want 8 bits for the message here
+    tty->c_cflag |= CS8;  //set data bits per message to 8
+    tty->c_cflag &= ~CRTSCTS; //Disable RTC/CTS hardware flow control
+    tty->c_cflag |= CREAD | CLOCAL; // enabeling CREAD makes the port able to read serial data? CLOCAL turn of controllines for modem translations?
+
+}
+void hpSerialRead::hpSetupLflag(struct termios *tty){
+    tty->c_lflag &= ~ICANON; //disable icanonical mode so that we dont process data line på line and so that we gather everything sent and do not treat special characters differently, in canonical inpus is prossesed when a new line is recieved
+    tty->c_lflag &= ~ISIG; //Disable interupts quit and suspends like ctrl-C
+
+}
+void hpSerialRead::hpSetupIflag(struct termios *tty){
+    tty->c_iflag &= ~(IXON | IXOFF | IXANY);                                      // disable flow controls allow for raw serial input to be read.
+    tty->c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL); //Disable any special handling of recived bytes    
+
+}
+void hpSerialRead::hpSetupCc(struct termios *tty){
+    tty->c_cc[VTIME] = 255;
+    tty->c_cc[VMIN] = 0;
+}
+
+
+
+
+/*READ FROM FILE
 hpSerialRead::hpSerialRead(std::string filepath){
     this->filepath=filepath;
 }
@@ -29,4 +89,4 @@ void hpSerialRead::read_from_fd(std::ifstream& fd){
 
 std::vector<uint8_t> hpSerialRead::getdata(){
     return this->data;
-}
+}*/
