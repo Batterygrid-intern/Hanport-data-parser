bcf91849c2bbb5bddfa1ba06dcfd9273129611eb
Ludvig <luddenorin@gmail.com>
2025-10-31 11:39:37 +0100

    changed writetest to read from filreader and interpret a hanport loop

--- tests/writetest.cpp
+++ tests/writetest.cpp
@@ -3,15 +3,16 @@
 #include <cstdint>
 #include "hpSerialRead.hpp"
 #include "CppLinuxSerial/SerialPort.hpp"
-
-  
-  
+#include "filereader.hpp"
+#include <thread>  
+#include <chrono>
   int main(){
-       hpSerialRead data_reader("./bin/../exampledata/meter_data.bin");
-       std::ifstream fd = data_reader.open_fd();
+       fileReader data_reader("./bin/../exampledata/meter_data.bin");
+       data_reader.open_f();
        /*TEST code
        ************************************************************************************/
-       data_reader.read_from_fd(fd);
+       data_reader.read_from_file();
+       std::vector<uint8_t> datatest = data_reader.getdata();
        // initialize serialport object to read and write data to serial port
        mn::CppLinuxSerial::SerialPort serialport("/dev/ttyAMA0",
                                                  mn::CppLinuxSerial::BaudRate::B_115200,
@@ -19,12 +20,11 @@
                                                  mn::CppLinuxSerial::Parity::NONE,
                                                  mn::CppLinuxSerial::NumStopBits::ONE);
        // open serial port and write to serial port
-       std::vector<uint8_t> datatest = data_reader.getdata();
-
        serialport.SetTimeout(25000);
        serialport.Open();
-       serialport.WriteBinary(datatest);
        while(true){
+         std::this_thread::sleep_for(std::chrono::seconds(10));
+         serialport.WriteBinary(datatest);
        }
 
    } 
