9d6a0691ec69aaa6ebf3daf78da9373b55bc67a5
Ludvig Norin <luddenorin@gmail.com>
2025-11-10 10:49:10 +0100

    changed som values

--- include/hpLogger.hpp
+++ include/hpLogger.hpp
@@ -1,14 +1,29 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <ctime>
 #include <string>
 
-
+//Loggin  levels DEBUG INFO WARN ERROR CRITICAL
+//Final destiantion where to outpput the loggs
+//Context and timestamps chronological context with timestamps
+//Setup dynamic flexibility to change log levels and destinations at runtime
+enum LogLevel {
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    CRITICAL
+};
 
 
 class hpLogger{
     public:
-
-
+        hpLogger(const std::string& filepath);
+        void log(LogLevel level, const std::string& message);
+        char* getTimeStamp();
     private:
+        std::ofstream logFile_;
+        std::string levelToString(LogLevel level);
+
 };
--- src/hpLogger.cpp
+++ src/hpLogger.cpp
@@ -1,2 +1,34 @@
-build a logger
-that reads all logs to a file with timestamps and log levels
+#include "hpLogger.hpp"
+
+
+//construct object and open filepath in append mode
+hpLogger::hpLogger(const std::string& filepath) {
+    logFile_.open(filepath, std::ios::app);
+    if (!logFile_.is_open()) {
+        std::cerr << "Failed to open log file: " << filepath << std::endl;
+    }
+}
+
+void hpLogger::log(LogLevel level, const std::string& message){
+    //get time stamp for loggmessage
+    const char* timestamp = getTimeStamp();
+
+    std::ostringstream logEntry;
+    logEntry << "[" << timestamp << "] "
+             << levelToString(level) << ": " << message
+             << std::endl;
+
+    if (logFile_.is_open()) {
+        logFile_ << logEntry.str();
+        logFile_.flush();
+    }
+}
+
+
+char* hpLogger::getTimeStamp(){
+    time_t now = time(0);
+    tm* timeInfo = localtime(&now);
+    char timestamp[20];
+    strftime(timestamp,sizeof(timestamp), "%Y-%m-%d %H:%M:%S",timeInfo);
+    return timestamp;
+}
